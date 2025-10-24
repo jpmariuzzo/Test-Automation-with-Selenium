d8e081feea8b77d0e5907d05465d3a57bf409da7
jpmariuzzo <jpmariuzzo@gmail.com>
2025-10-24 15:52:59 -0300

    Selenium concepts with Python.

---
+++ actionsDemo.py
@@ -0,0 +1,20 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.ie.service import Service
+
+service_obj = Service(r"C:\Users\I009111\PycharmProjects\pythonTesting\pythonSelenium\chromedriver-win64\chromedriver-win64\chromedriver.exe")
+driver = webdriver.Chrome(service=service_obj)
+
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+driver.maximize_window()
+action = ActionChains(driver)
+
+action.move_to_element(driver.find_element(By.ID, "mousehover")).perform()
+#action.context_click(driver.find_element(By.LINK_TEXT, "Top")).perform()
+action.move_to_element(driver.find_element(By.LINK_TEXT, "Reload")).click().perform()
+
+
+time.sleep(5)
---
+++ childWindow.py
@@ -0,0 +1,25 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.ie.service import Service
+
+service_obj = Service(r"C:\Users\I009111\PycharmProjects\pythonTesting\pythonSelenium\chromedriver-win64\chromedriver-win64\chromedriver.exe")
+driver = webdriver.Chrome(service=service_obj)
+
+driver.get("https://the-internet.herokuapp.com/windows")
+driver.maximize_window()
+
+driver.find_element(By.LINK_TEXT, "Click Here").click()
+
+windowsOpened = driver.window_handles # Creates a list of opened windows
+driver.switch_to.window(windowsOpened[1]) ## Switches to the new window, [0] to [1]
+print(driver.find_element(By.TAG_NAME, "h3").text)
+assert "New Window" == driver.find_element(By.TAG_NAME, "h3").text
+driver.close()
+driver.switch_to.window(windowsOpened[0])
+print(driver.find_element(By.TAG_NAME, "h3").text)
+assert "Opening a new window" == driver.find_element(By.TAG_NAME, "h3").text
+
+time.sleep(3)
